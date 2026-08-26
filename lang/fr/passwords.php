b37de2916395e6fb1143881236002b4a8deddc55
Niels <niels.netelenbos@gmail.com>
2026-08-26 09:38:49 +0000

    Francisation: locale fr, messages validation/auth/pagination en francais

---
+++ lang/fr/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Votre mot de passe a été réinitialisé.',
+    'sent' => 'Un e-mail de réinitialisation du mot de passe a été envoyé.',
+    'throttled' => 'Merci de patienter avant de réessayer.',
+    'token' => 'Ce lien de réinitialisation est invalide ou expiré.',
+    'user' => 'Aucun compte n\'est associé à cette adresse e-mail.',
+];
